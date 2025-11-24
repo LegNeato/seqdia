02b19ce031949cd091bda01359f543c35d70fada
Christian Legnitto <christian@legnitto.com>
2025-11-23 18:49:28 -0800

    Point demo UI utilities at library helper

--- apps/demo/components/ui/separator.tsx
+++ apps/demo/components/ui/separator.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "../../lib/utils";
+import { cn } from "../../packages/seqdia/src/lib/utils";
 
 const Separator = React.forwardRef<
   HTMLDivElement,
