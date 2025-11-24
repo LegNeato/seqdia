8042ef7d7c682cbd77ab1ff65c4b70f3ea050755
Christian Legnitto <christian@legnitto.com>
2025-11-23 19:08:58 -0800

    Export granular layout layers and layout hook

--- packages/seqdia/src/index.ts
+++ packages/seqdia/src/index.ts
@@ -1,8 +1,16 @@
 export { SequenceDiagram } from "./components/sequence/SequenceDiagram";
+export {
+  HeaderGrid,
+  RegionsLayer,
+  RailsLayer,
+  MessagesLayer,
+} from "./components/sequence/SequenceDiagram";
 export { SequenceProvider } from "./components/sequence/SequenceProvider";
 export { useSequenceApi } from "./hooks/useSequenceApi";
 export { useSequenceController } from "./hooks/useSequenceController";
 export type { SequenceController } from "./hooks/useSequenceController";
+export { useSequenceLayout } from "./hooks/useSequenceLayout";
+export { COLUMN_WIDTH } from "./lib/constants";
 export * from "./lib/sequence/types";
 export * from "./lib/sequence/layout";
 export * from "./lib/sequence/visible";
